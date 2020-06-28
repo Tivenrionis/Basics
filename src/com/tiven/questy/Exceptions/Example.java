1366d5d79ee6aacfd98c2feae16026218cff605a
Tiven <tivenstudio@gmail.com>
2020-06-28 23:22:19 +0200

    clean up the code :: exception

--- src/com/tiven/questy/Exceptions/Example.java
+++ src/com/tiven/questy/Exceptions/Example.java
@@ -7,8 +7,14 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division");
+        }
     }
 
     private static int divide() {
@@ -16,15 +22,14 @@ public class Example {
         try {
             x = getInt();
             y = getInt();
+            System.out.println("x is " + x + " , y is " + y);
+            return x / y;
         } catch (NoSuchElementException e) {
             throw new ArithmeticException("wrong input");
-        }
-        System.out.println("x is " + x + " , y is " + y);
-        try {
-            return x / y;
         } catch (ArithmeticException e) {
             throw new ArithmeticException("Cant divide by zero");
         }
+
     }
 
     private static int getInt() {
