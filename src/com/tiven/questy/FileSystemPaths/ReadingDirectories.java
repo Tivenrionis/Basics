bef88bf6e6b48bb29cff70175ce821c5e0e89d97
Tiven <tivenstudio@gmail.com>
2020-07-10 14:31:43 +0200

    FileStores, file separators, rootpaths

--- src/com/tiven/questy/FileSystemPaths/ReadingDirectories.java
+++ src/com/tiven/questy/FileSystemPaths/ReadingDirectories.java
@@ -1,5 +1,6 @@
 package com.tiven.questy.FileSystemPaths;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -13,7 +14,8 @@ public class ReadingDirectories {
         };
         DirectoryStream.Filter<Path> filter1 = p -> Files.isRegularFile(p);
 
-        Path directory = FileSystems.getDefault().getPath("Examples\\Dir2");
+        //  Path directory = FileSystems.getDefault().getPath("Examples\\Dir2");
+        Path directory = FileSystems.getDefault().getPath("Examples" + File.separator + "Dir2");
 //implements Iterable so u can iterate
         // try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory,"*.dat")) {
         try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter1)) {
@@ -23,6 +25,30 @@ public class ReadingDirectories {
         } catch (IOException | DirectoryIteratorException e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
+        }
+
+        //NEVER HARDCODE SEPARATOR
+        String sep = File.separator;
+        System.out.println(sep);
+        sep = FileSystems.getDefault().getSeparator();
+        System.out.println(sep);
+
+        try {
+            Path temp = Files.createTempFile("myapp", "appext");
+            System.out.println("Temp is on " + temp.toAbsolutePath());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for (FileStore store : stores) {
+            System.out.println(store);
+            System.out.println(store.name());
+        }
+        System.out.println("========================");
+        Iterable<Path> rootPaths = FileSystems.getDefault().getRootDirectories();
+        for (Path path : rootPaths) {
+            System.out.println(path);
         }
 
     }
