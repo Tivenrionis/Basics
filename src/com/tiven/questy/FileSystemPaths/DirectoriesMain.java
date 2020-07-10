31892aa0755b5112076e9227edc0d1bf4cb4afdf
Tiven <tivenstudio@gmail.com>
2020-07-10 13:15:58 +0200

    deleting files

--- src/com/tiven/questy/FileSystemPaths/DirectoriesMain.java
+++ src/com/tiven/questy/FileSystemPaths/DirectoriesMain.java
@@ -8,10 +8,13 @@ import java.nio.file.Path;
 public class DirectoriesMain {
     public static void main(String[] args) {
         try {
-            //Moving - renaming
-            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path destination = FileSystems.getDefault().getPath("Examples", "file2.txt");
-            Files.move(fileToMove, destination);
+
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "filecopy.txt");
+            Files.deleteIfExists(fileToDelete);
+//            //Moving - renaming
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.move(fileToMove, destination);
 //            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
 //            Path copyFile = FileSystems.getDefault().getPath("Examples", "filecopy.txt");
 //            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
