41c783e2bbe5a1408fc1ecffd1c4d4ae13031388
Tiven <tivenstudio@gmail.com>
2020-07-11 13:40:07 +0200

    Waliking file tree using SimpleFileVisitor

--- src/com/tiven/questy/FileSystemPaths/ReadingDirectories.java
+++ src/com/tiven/questy/FileSystemPaths/ReadingDirectories.java
@@ -51,5 +51,14 @@ public class ReadingDirectories {
             System.out.println(path);
         }
 
+        System.out.println("Walking Tree for Dir2");
+        Path dir2Path = FileSystems.getDefault().getPath("Examples", File.separator, "Dir2");
+        try {
+            Files.walkFileTree(dir2Path, new PrintNames());
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
