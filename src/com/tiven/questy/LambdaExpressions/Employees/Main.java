be4c0d5ae728b3989a1e94fc8e21d4a764136d47
Tiven <tivenstudio@gmail.com>
2020-08-04 10:54:44 +0200

    Functions, lambdas are badass

--- src/com/tiven/questy/LambdaExpressions/Employees/Main.java
+++ src/com/tiven/questy/LambdaExpressions/Employees/Main.java
@@ -5,14 +5,14 @@ package com.tiven.questy.LambdaExpressions.Employees;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.IntPredicate;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 //parametry -> cialo
 
-// Predicate :: warunki
+// Predicate :: warunki true or false
 // Supplier :: bez argumentow ale zwraca wartosci
+//Consumer :: nie zwraca wartosci
 public class Main {
     public static void main(String[] args) {
         Employee john = new Employee("John Buchalka", 22);
@@ -30,33 +30,59 @@ public class Main {
         employeeList.add(snow);
         employeeList.add(red);
         employeeList.add(charmin);
-        printEmployeesByAge(employeeList, "Over 30", employee -> employee.getAge() > 30);
-        printEmployeesByAge(employeeList, "Less than 30", employee -> employee.getAge() <= 30);
-        printEmployeesByAge(employeeList, "Less than 25 using anonymous", new Predicate<Employee>() {
-            @Override
-            public boolean test(Employee employee) {
-                return employee.getAge() < 25;
+
+        // parameter/ return type
+        Function<Employee, String> getLastName = employee -> employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        Function<Employee, String> getFirstName = employee -> employee.getName().substring(0, employee.getName().indexOf(' '));
+
+        String lastName = getLastName.apply(employeeList.get(2));
+        //  System.out.println(lastName);
+        Random randomBoolean = new Random();
+        for (Employee employee : employeeList) {
+            if (randomBoolean.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
             }
-        });
 
-        IntPredicate greaterThan15 = i -> i > 15;
-        IntPredicate lessThan100 = i -> i < 100;
-        System.out.println(greaterThan15.test(10));
-        int a = 15;
-        System.out.println(greaterThan15.test(a + 5));
-        System.out.println(greaterThan15.and(lessThan100).test(333));
-
-
-        Random random = new Random();
-        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(randomSupplier.get());
         }
+
+//        printEmployeesByAge(employeeList, "Over 30", employee -> employee.getAge() > 30);
+//        printEmployeesByAge(employeeList, "Less than 30", employee -> employee.getAge() <= 30);
+//        printEmployeesByAge(employeeList, "Less than 25 using anonymous", new Predicate<Employee>() {
+//            @Override
+//            public boolean test(Employee employee) {
+//                return employee.getAge() < 25;
+//            }
+//        });
+//
+//        IntPredicate greaterThan15 = i -> i > 15;
+//        IntPredicate lessThan100 = i -> i < 100;
+//        System.out.println(greaterThan15.test(10));
+//        int a = 15;
+//        System.out.println(greaterThan15.test(a + 5));
+//        System.out.println(greaterThan15.and(lessThan100).test(333));
+//
+//
+//        Random random = new Random();
+//        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
 //        for (int i = 0; i < 10; i++) {
-//            System.out.println(random.nextInt(1000));
+//            System.out.println(randomSupplier.get());
 //        }
+////        for (int i = 0; i < 10; i++) {
+////            System.out.println(random.nextInt(1000));
+////        }
+//
+//        employeeList.forEach(employee -> {
+//            String lastName = employee.getName().substring(employee.getName().indexOf(' ') + 1);
+//            System.out.println("Last name is " + lastName);
+//        });
 
 
+    }
+
+    private static String getAName(Function<Employee, String> function, Employee employee) {
+        return function.apply(employee);
     }
 
     private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
