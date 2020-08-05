1000a302ed20b48386ea13651f3913efad6c2821
Tiven <tivenstudio@gmail.com>
2020-08-05 12:01:42 +0200

    collect,reduce,flatMap, lazy evaluation on streams

--- src/com/tiven/questy/Streams/Main.java
+++ src/com/tiven/questy/Streams/Main.java
@@ -3,6 +3,8 @@ package com.tiven.questy.Streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -41,6 +43,27 @@ public class Main {
                 .distinct()
                 .peek(System.out::println) // Intermediate operation -> niby zwraca void przy Consumerze  ale przepisuje wartosci i zwraca nowy stream
                 .count());
+        System.out.println("=============================");
+        List<String> sortedNumbers = bingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(Collectors.toList());
+
+        sortedNumbers.forEach(System.out::println);
+        System.out.println("=============================");
+
+        System.out.println("=============================");
+        List<String> sortedNumbers2 = bingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        sortedNumbers2.forEach(System.out::println);
+        System.out.println("=============================");
 
 
 // Mozna to ogarnac  wjednej lini
@@ -53,5 +76,40 @@ public class Main {
 
         gNumbers.sort((s1, s2) -> s1.compareTo(s2));
         gNumbers.forEach(bingoNumber -> System.out.println(bingoNumber));*/
+
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jack = new Employee("Jack Deer", 25);
+        Employee sparrow = new Employee("Jack Sparrow", 40);
+        Employee snow = new Employee("John Snow", 22);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(john);
+        hr.addEmployee(jack);
+        hr.addEmployee(sparrow);
+
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(snow);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments
+                .stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+        Map<Integer, List<Employee>> groupedByAge = departments
+                .stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+// OPERATION IN STREAMS ARE LAZLY EVALUATED ---> INTERMEDIATE OPERATIONS ARE NOT EVALUATED UNTIL THERES A TERMINAL OPERATION
     }
 }
