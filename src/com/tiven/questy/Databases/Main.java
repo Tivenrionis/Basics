6c11c95a2168b969473e23ee280a7762dc932e99
Tiven <tivenstudio@gmail.com>
2020-08-21 12:47:28 +0200

    Ready to go with databases

---
+++ src/com/tiven/questy/Databases/Main.java
@@ -0,0 +1,7 @@
+package com.tiven.questy.Databases;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/tiven/questy/DebugingAndTesting/JUnit/Challange/Main.java
+++ src/com/tiven/questy/DebugingAndTesting/JUnit/Challange/Main.java
@@ -1,7 +1,12 @@
 package com.tiven.questy.DebugingAndTesting.JUnit.Challange;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String[]> test = Arrays.asList(new String[][]{{"elo", "melo"}, {"siema", "nara"}});
+        System.out.println();
 
     }
 }
