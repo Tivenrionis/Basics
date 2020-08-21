8912e549a7154cddb5d0d5e5ffb6571f51a003fa
Tiven <tivenstudio@gmail.com>
2020-08-21 13:44:49 +0200

    query results example standard way

--- src/com/tiven/questy/Databases/Main.java
+++ src/com/tiven/questy/Databases/Main.java
@@ -1,9 +1,6 @@
 package com.tiven.questy.Databases;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 //2 ways to get connection
 // DriverManager or DataSource objects -> more flexible and used in EE apps
@@ -21,10 +18,28 @@ public class Main {
         }*/
         try {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\tiven\\DATABASES\\testjava.db");
+            //connection.setAutoCommit(false);
             Statement statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
-                            "(name VARCHAR(255),phone INT, email TEXT)");
+                    "(name VARCHAR(255),phone INT, email TEXT)");
 
+//            statement.execute("INSERT INTO contacts VALUES ('Julia','4252222','vdd@mail.com')");
+//            statement.execute("INSERT INTO contacts VALUES ('Michal','256662','tiv@mail.com')");
+//            statement.execute("INSERT INTO contacts VALUES ('Pola','6321222','polpol@mail.com')");
+//            statement.execute("INSERT INTO contacts VALUES ('Rozalia','9726465','rozbloz@mail.com')");
+//
+//            statement.execute("UPDATE contacts SET name='TIVEN' WHERE name='Rozalia'");
+//            statement.execute("DELETE FROM contacts WHERE name = 'TIVEN'");
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()) {
+                System.out.println(results.getString("name") + " " +
+                        results.getInt("phone") + " " +
+                        results.getString("email"));
+            }
+
+            results.close();
             statement.close();
             connection.close();
 
