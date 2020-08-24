0107d226c7519394ab8f91d33080fa19f6497f0f
Tiven <tivenstudio@gmail.com>
2020-08-24 13:44:30 +0200

    reading html from the page

--- src/com/tiven/questy/Networking/HighLevelAPI/Main.java
+++ src/com/tiven/questy/Networking/HighLevelAPI/Main.java
@@ -1,5 +1,8 @@
 package com.tiven.questy.Networking.HighLevelAPI;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -8,16 +11,19 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) {
         try {
-            URI baseUri = new URI("http://username:password@myserver.com:5000");
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            //URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            // URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            URL url = new URL("http://example.org");
+            URI uri = url.toURI();
 
-            URI resolvedUri = baseUri.resolve(uri);
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(url.openStream()));
 
-            URL url = resolvedUri.toURL();
-            System.out.println("URL = " + url);
-//
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
+
+
 //            System.out.println("Scheme = " + uri.getScheme());
 //            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
 //            System.out.println("Authority = " + uri.getAuthority());
@@ -27,11 +33,42 @@ public class Main {
 //            System.out.println("Path = " + uri.getPath());
 //            System.out.println("Query = " + uri.getQuery());
 //            System.out.println("Fragment = " + uri.getFragment());
+//
+//            URI baseUri = new URI("http://username:password@mynewserver.com:5000");
+//            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
+//            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
+//            URI uri3 = new URI("/store/locations?zip=12345");
+//
+//            //URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+//            // URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+//
+//            URI resolvedUri1 = baseUri.resolve(uri1);
+//            URI resolvedUri2 = baseUri.resolve(uri2);
+//            URI resolvedUri3 = baseUri.resolve(uri3);
+//
+//            URL url1 = resolvedUri1.toURL();
+//            System.out.println("URL = " + url1);
+//            URL url2 = resolvedUri2.toURL();
+//            System.out.println("URL = " + url2);
+//            URL url3 = resolvedUri3.toURL();
+//            System.out.println("URL = " + url3);
+//
+//
+//
+//            URI relativizedURI= baseUri.relativize(resolvedUri2);
+//            System.out.println("Relative URI = "+ relativizedURI);
+//
+//
+//
+////
 
+//
         } catch (URISyntaxException e) {
             System.out.println("URI bad syntax " + e.getMessage());
         } catch (MalformedURLException e) {
             System.out.println("URL malformed " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
