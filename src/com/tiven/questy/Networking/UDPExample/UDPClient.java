1e9df1443cfb5f862fc6e45dbe5775ba952b8fd2
Tiven <tivenstudio@gmail.com>
2020-08-24 11:47:12 +0200

    UDP server/client example

---
+++ src/com/tiven/questy/Networking/UDPExample/UDPClient.java
@@ -0,0 +1,44 @@
+package com.tiven.questy.Networking.UDPExample;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+public class UDPClient {
+    public static void main(String[] args) {
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            DatagramSocket socket = new DatagramSocket();
+
+            Scanner scanner = new Scanner(System.in);
+
+            String echoString;
+
+            do {
+                System.out.println("Enter string to echo:");
+                echoString = scanner.nextLine();
+
+                byte[] buffer = echoString.getBytes();
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5000);
+
+                socket.send(packet);
+
+                byte[] buffer2 = new byte[50];
+                packet = new DatagramPacket(buffer2, buffer2.length);
+                socket.receive(packet);
+
+                System.out.println("Text received via UDP " + new String(buffer2, 0, packet.getLength()));
+
+            } while (!echoString.equals("exit"));
+
+        } catch (SocketTimeoutException e) {
+            System.out.println("Socket timed out");
+        } catch (IOException e) {
+            System.out.println("ClientError " + e.getMessage());
+        }
+    }
+}
